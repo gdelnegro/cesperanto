54e60350649c935d8c80f6a484dc19cafaa6af72
root <root@bwf.(none)>
2014-01-14 23:43:06 +0000

    lol

---
+++ templates_c/%%0E^0E4^0E407559%%footer.tpl.php
@@ -0,0 +1,38 @@
+<?php /* Smarty version 2.6.26, created on 2014-01-14 22:04:51
+         compiled from footer.tpl */ ?>
+<?php if ($this->_tpl_vars['g_demo'] == 'True'): ?>
+<script type="text/javascript"><!--
+ google_ad_client = "ca-pub-3696425351841264";
+ /* 728x90_ODM_Demo */
+ google_ad_slot = "8419809005";
+ google_ad_width = 728;
+ google_ad_height = 90;
+ //-->
+ </script>
+ <script type="text/javascript"
+ src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+ </script>
+<?php endif; ?>
+      <hr>
+
+      <footer>
+          <a href="http://www.opendocman.com/">
+                <img src="<?php echo $this->_tpl_vars['g_base_url']; ?>
+/images/logo.gif" title="<?php echo $this->_tpl_vars['site_title']; ?>
+" alt="<?php echo $this->_tpl_vars['site_title']; ?>
+" border="0">
+            </a>
+        <p>Copyright &copy; 2000-2013 Stephen Lawrence</p>
+        <p><a href="http://www.opendocman.com/" target="_new">OpenDocMan v<?php echo $this->_tpl_vars['g_current_version']; ?>
+</a> | 
+            <a href="http://www.opendocman.com/forum/" target="_new">Support</a> |
+            <a href="http://opendocman.uservoice.com" target="_new">Feedback</a> |
+            <a href="https://github.com/opendocman/opendocman/issues" target="_new">Bugs</a> |
+        </p>
+      </footer>
+
+    </div> <!-- /container -->   
+    <script type="text/javascript" src="<?php echo $this->_tpl_vars['g_base_url']; ?>
+/templates/tweeter/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ templates_c/%%FB^FB5^FB557B53%%_add_footer.tpl.php
@@ -0,0 +1,11 @@
+<?php /* Smarty version 2.6.26, created on 2014-01-14 22:04:52
+         compiled from /var/www/opendoc//templates/common/_add_footer.tpl */ ?>
+    
+
+
+        <tr>
+            <td colspan="3" align="center"><div class="buttons"><button class="positive" tabindex=7 type="Submit" name="submit" value="Add Document"><?php echo $this->_tpl_vars['g_lang_submit']; ?>
+</button></div></td>
+        </tr>
+    </table>
+</form>
